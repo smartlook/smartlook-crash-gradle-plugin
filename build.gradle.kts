639348054889c44f4e34ac20d9370723112392f1
ryzzaki <cuong.nugi@gmail.com>
2021-10-01 16:42:41 +0100

    feat: prepared gradle file for publishing to central repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   `java-gradle-plugin`
   id("org.jetbrains.kotlin.jvm") version "1.4.31"
   id("maven-publish")
+  id("signing")
 }
 
 repositories {
@@ -19,6 +20,9 @@ dependencies {
 java {
   sourceCompatibility = JavaVersion.VERSION_1_8
   targetCompatibility = JavaVersion.VERSION_1_8
+
+  withSourcesJar()
+  withJavadocJar()
 }
 
 group = "com.smartlook"
@@ -31,4 +35,58 @@ gradlePlugin {
     implementationClass = "com.smartlook.plugins.src.SmartlookUploadPlugin"
   }
   println("Plugin '${smartlookCrashGradlePlugin.id}' has been defined with implementation class '${smartlookCrashGradlePlugin.implementationClass}'")
+}
+
+apply(plugin = "maven-publish")
+apply(plugin = "signing")
+
+publishing {
+  repositories {
+    maven {
+      val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+      val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+      url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+
+      credentials {
+        username=project.properties["ossrhUsername"].toString()
+        password=project.properties["ossrhPassword"].toString()
+      }
+    }
+  }
+
+  publications {
+    create<MavenPublication>("maven") {
+      pom {
+        name.set("smartlook-crash-gradle-plugin")
+        description.set("The Smartlook Crash Gradle Plugin will automatically find the mappings of all your build types and upload them.")
+        url.set("https://www.smartlook.com/")
+
+        // @To-Do: Define the licenses properly
+        licenses {
+          license {
+            name.set("...")
+            url.set("...")
+          }
+        }
+
+        developers {
+          developer {
+            id.set("pajk")
+            name.set("Pavel Pokorny")
+            email.set("pavel.pokorny@smartlook.com")
+          }
+        }
+
+        scm {
+          connection.set("git@github.com:smartlook/smartlook-crash-gradle-plugin.git")
+          developerConnection.set("git@github.com:smartlook/smartlook-crash-gradle-plugin.git")
+          url.set("https://github.com/smartlook/smartlook-crash-gradle-plugin.git")
+        }
+      }
+    }
+  }
+}
+
+signing {
+  sign(configurations.archives.get())
 }
